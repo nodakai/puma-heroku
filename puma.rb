e86d46ed4d64f58e0602d9e9caf18413e0ef35e2
NODA, Kai <nodakai@gmail.com>
2014-08-06 10:56:05 +0800

    No, the app should look at ENV['PORT']

--- puma.rb
+++ puma.rb
@@ -1,6 +1,10 @@
 
 # encoding: utf-8
 
+preload_app!
+
+port ENV['PORT'] || 3000
+
 app {|env|
     body = 'こんにちは、ヘロクとプーマ。'
     [200, {'Content-Type' => 'text/plain', 'Content-Length' => body.bytesize.to_s }, [body]]
